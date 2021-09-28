dd5fbe948c57485f222b33bf1e097b88c5dbbabd
TonkoshkuraMisha <tonkoshkura.misha@gmail.com>
2021-09-28 13:58:35 +0300

    main.py file was changed

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+def pretty_print(mas):
+    print('-' * 10)
+    for row in mas:
+        print(*row)
+    print('-' * 10)
+
+
+def get_number_from_index(i, j):
+    return 4 * i + j + 1
+
+
+def get_empty_list(mas):
+    empty = []
+    for i in range(4):
+        for j in range(4):
+            if mas[i][j] == 0:
+                num = get_number_from_index(i, j)
+                empty.append(num)
+    return empty
+
+
 mas = [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
@@ -5,6 +26,7 @@ mas = [
     [0, 0, 0, 0]
 ]
 
-mas [2][1] = 2
-mas [3][0] = 4
-print(mas)
+mas[1][2] = 2
+mas[3][0] = 4
+print(get_empty_list(mas))
+pretty_print(mas)
